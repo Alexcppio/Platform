cef513f85d76c334356a91b0d5e7f50e5e4f7150
Alexcppio <test21112019test@gmail.com>
2021-10-08 15:06:14 +0500

    some code

--- Startup.cs
+++ Startup.cs
@@ -7,13 +7,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace Platform
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -26,8 +25,17 @@ namespace Platform
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Method == HttpMethods.Get)
+                {
+                    await context.Response.WriteAsync("Custom Middleware \n");
+                    await context.Response.WriteAsync(context.Response.ContentLength.ToString() + "\n");
+                }
+                await next();
+            });
+            
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
