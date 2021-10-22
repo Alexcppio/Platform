0bdb3d199bcf817acf6c5529d2608c83ebbd0628
Alexcppio <test21112019test@gmail.com>
2021-10-22 10:40:25 +0500

    add journal messages

--- Startup.cs
+++ Startup.cs
@@ -9,46 +9,60 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.FileProviders;
 
 namespace Platform
 {
     public class Startup
     {
-        public Startup(IConfiguration config)
+        public Startup(IConfiguration configService)
         {
-            Configuration = config;
+            Configuration = configService;
         }
-        private IConfiguration Configuration;
+        private IConfiguration Configuration { get; set; }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(typeof(ICollection<>), typeof(List<>));
+            services.Configure<MessageOptions>(Configuration.GetSection("Location"));
         }
-        public void Configure(IApplicationBuilder app, 
-                              IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app,
+                                IWebHostEnvironment env,
+                                ILogger<Startup> logger)
         {
-            app.UseDeveloperExceptionPage();            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new
+                PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"),
+                RequestPath = "/files"
+            });
             app.UseRouting();
+            app.Use(async (context, next) =>
+            {
+                string defaultDebug = Configuration["Logging:LogLevel:Default"];
+                await context.Response
+                .WriteAsync($"The config setting is: {defaultDebug}");
+                string environ = Configuration["ASPNETCORE_ENVIRONMENT"];
+                await context.Response
+                .WriteAsync($"\nThe env setting is: {environ}");
+                string wsID = Configuration["WebService:Id"];
+                string wsKey = Configuration["WebService:Key"];
+                await context.Response.WriteAsync($"\nThe secret ID is: {wsID}");
+                await context.Response.WriteAsync($"\nThe secret Key is {wsKey}");
+            });
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/string", async context =>
+                endpoints.MapGet("/", async context =>
                 {
-                    ICollection<string> collection
-                    = context.RequestServices.GetService<ICollection<string>>();
-                    collection.Add($"Request: {DateTime.Now.ToLongTimeString()}");
-                    foreach(string str in collection)
-                    {
-                        await context.Response.WriteAsync($"String: {str}\n");
-                    }
-                });
-                endpoints.MapGet("/int", async context =>
-                {
-                    ICollection<int> collection
-                    = context.RequestServices.GetService<ICollection<int>>();
-                    collection.Add(collection.Count() + 1);
-                    foreach(int val in collection)
-                    {
-                        await context.Response.WriteAsync($"Int: {val}\n");
-                    }
+                    logger.LogDebug("Response for / started");
+                    await context.Response.WriteAsync("Hello World!");
+                    logger.LogDebug("Response for / completed");
                 });
             });
         }
